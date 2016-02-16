01cf58132afd0df6171a57abd07d7d33513eb825
Will CS <will@apressci.co.uk>
2016-02-16 23:34:16 +0000

    integrated (and commented out) gpio stuff

---
+++ sonos_buttons/classes.py
@@ -0,0 +1,133 @@
+import parameters as params
+#import RPi.GPIO as GPIO
+import time
+
+#GPIO.setmode(GPIO.BOARD)
+
+class ButtonList(list):
+
+    def __init__(self):        
+        pass
+
+    # Add a new button to this list of buttons, but also
+    # return the new button, so we can capture a handle to it
+    # in the calling module.
+
+    def add(self, name, pin_number):
+        item_count = len(self)
+        button = Button(name, pin_number, 2**item_count)
+        self.append(button)
+        return button
+
+    def get_pressed_buttons(self):
+        return sum([button.mask for button in self if button.is_pressed()])
+
+    def get_button_event(self):
+
+        initial_buttons = self.get_pressed_buttons()
+        if initial_buttons:
+
+            start_time = time.time()
+
+            while True:
+
+                current_buttons = self.get_pressed_buttons()
+                if initial_buttons > current_buttons:
+                    # buttons were released, so return the event
+                    return ButtonEvent(initial_buttons, duration)
+                elif initial_buttons < current_buttons:
+                    # more buttons pressed, so start again
+                    return self.get_button_event()
+                duration = time.time() - start_time
+                if duration > params.MAX_PRESS_DURATION:
+                    # max duration exceeded, so return the event
+                    return ButtonEvent(initial_buttons, duration)
+
+        else:
+
+            return None
+
+class Button():
+
+    def __init__(self, name, pin_number, mask):
+        self.name = name
+        self.pin_number = pin_number
+        self.mask = mask
+        #GPIO.setup(self.pin_number, GPIO.IN)
+
+    def is_pressed(self):
+        if self.pin_number in [17]:
+            return True
+        else:
+            return False
+
+    # Note that these magic functions allow us to use
+    # the button's 'mask' value for == tests and + operations.
+
+    def __eq__(self, other):
+        if isinstance(other, Button):
+            return (self.mask==other.mask)
+        return (self.mask==other)
+
+    def __add__(self, other):
+        if isinstance(other, Button):
+            return (self.mask + other.mask)
+        else:
+            return (self.mask + other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+class ButtonEvent():
+
+    def __init__(self, buttons, duration):
+        self.buttons = buttons
+        self.duration = duration
+
+    def is_long_press(self):
+        if self.duration > params.LONG_PRESS_DURATION:
+            return True
+        else:
+            return False
+
+    def is_button_pressed(self, button):
+        return bool(self.buttons & button.mask)
+
+class LEDList(list):
+
+    def __init__(self):
+        pass
+
+    def add(self, name, pin_number):
+        led = LED(name, pin_number)
+        self.append(led)
+        return led
+
+class LED():
+
+    def __init__(self, name, pin_number):
+        self.name = name
+        self.pin_number = pin_number
+        self._is_on = False
+        #GPIO.setup(self.pin_number, GPIO.OUT)
+    
+    def turn_on(self):
+        #GPIO.output(self.pin_number,True)
+        self._is_on = True
+    
+    def turn_off(self):
+        #GPIO.output(self.pin_number, False)
+        self._is_on = False
+
+    def is_on(self):
+        return self._is_on
+
+    def flash(self, count=2, gap=0.1):
+        if self.is_on():
+            self.turn_off
+
+        for i in range(0, count):
+            self.turn_on()
+            time.sleep(gap)
+            self.turn_off()
+            time.sleep(gap)
--- sonos_buttons/sonos_buttons.py
+++ sonos_buttons/sonos_buttons.py
@@ -1,13 +1,11 @@
-from buttons import *
+from classes import *
 import parameters as params
 from sonosmemorycontroller import SonosMemoryController
 import logging
-import time
 
 # Set up the buttons. Make a button set and add buttons,
 # retaining handles to each one, for use later.
 buttons = ButtonList()
-
 BTN_1 = buttons.add('Button 1', 7)
 BTN_2 = buttons.add('Button 2', 13)
 BTN_3 = buttons.add('Button 3', 17)
@@ -15,43 +13,27 @@ BTN_4 = buttons.add('Button 4', 21)
 BTN_5 = buttons.add('Button 5', 24)
 BTN_6 = buttons.add('Button 6', 29)
 
+leds = LEDList()
+LED_1 = leds.add('LED Left', 14)
+LED_2 = leds.add('LED Right', 16)
+
 smc = SonosMemoryController()
 
-def get_pressed_buttons():
+def monitor_buttons():
 
-    return sum([button.mask for button in buttons if gpio_test_pin(button.pin_number)])
+    while True:
 
-def gpio_test_pin(pin_number):
-    #raise NotImplementedError("Nick to implement this")
-    if pin_number in [17]:
-        return True
-    else:
-        return False
+        button_event = buttons.get_button_event()
+        if button_event:
 
-def get_button_event():
+            logging.info("Button press detected.")
 
-    initial_buttons = get_pressed_buttons()
-    if initial_buttons:
+            trigger_action(button_event)
 
-        start_time = time.time()
-
-        while True:
-
-            current_buttons = get_pressed_buttons()
-            if initial_buttons > current_buttons:
-                # buttons were released, so return the event
-                return ButtonEvent(initial_buttons, duration)
-            elif initial_buttons < current_buttons:
-                # more buttons pressed, so start again
-                return get_button_event()
-            duration = time.time() - start_time
-            if duration > params.MAX_PRESS_DURATION:
-                # max duration exceeded, so return the event
-                return ButtonEvent(initial_buttons, duration)
-
-    else:
-
-        return None
+            # don't resume looping until all 
+            # buttons are released
+            while buttons.get_pressed_buttons():
+                pass
 
 
 def trigger_action(button_event):
@@ -61,9 +43,7 @@ def trigger_action(button_event):
     # sets of buttons. This will do bitwise matching against the buttons'
     # mask values.
 
-    # self.current_worker.stop()
-    # self.current_worker.
-
+    # in case the smc still has a thread running from earlier, cancel it
     smc.cancel_running_thread()
 
     if button_event.buttons == BTN_3:
@@ -80,23 +60,6 @@ def trigger_action(button_event):
         print('Unknown button combination held for {} seconds.'.format(button_event.duration))
         for button in buttons:
             print('Button: {}. Pressed: {}.'.format(button.name, button_event.is_button_pressed(button)))
-
-
-def monitor_buttons():
-
-    while True:
-
-        button_event = get_button_event()
-        if button_event:
-
-            logging.info("Button press detected.")
-
-            trigger_action(button_event)
-
-            # don't resume looping until all 
-            # buttons are released
-            while get_pressed_buttons():
-                pass
 
 if __name__=='__main__':
 
